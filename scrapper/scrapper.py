8e4c0e4017df07093c7ca466f623a5f94053b78b
iboliq <ibrahimdinc1999@gmail.com>
2021-01-14 20:08:42 +0300

    geckodriver selector

--- scrapper/scrapper.py
+++ scrapper/scrapper.py
@@ -7,7 +7,15 @@ class ScrapperClass:
         options = Options()
         options.headless = True
 
-        self.driver = webdriver.Firefox(executable_path="geckodriver.exe", options=options)
+        driverPath = ""
+
+        from sys import platform
+        if platform == "linux" or platform == "linux2":
+            driverPath = "geckodriver"
+        elif platform == "win32":
+            driverPath = "geckodriver.exe"
+
+        self.driver = webdriver.Firefox(executable_path=driverPath, options=options)
         super().__init__()
 
     def closeDriver(self):
