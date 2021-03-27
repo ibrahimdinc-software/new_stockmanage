bad0776c9aadc65f040317682c7b5719b23ff64a
iboliq <ibrahimdinc1999@gmail.com>
2021-03-28 01:38:46 +0300

    cron update

--- storage/cron.py
+++ storage/cron.py
@@ -1,22 +1,18 @@
-from trendyol_api.tr_module import OrderModule as tr_Order, ProductModule as tr_Product
-from hepsiburada_api.hb_module import OrderModule as hb_Order, ProductModule as hb_Product
+
+from marketplace.module import ProductModule,OrderModule
 
 from new_stockmanage.mail import loseBuyboxMail
 
 def productUpdate():
-    hb_Product().updateProducts()
-    tr_Product().updateProducts()
+    ProductModule().updateProducts()
 
 def getOrders():
-    tr_Order().getOrders()
-    hb_Order().getOrders()
+    OrderModule().getOrders()
     productUpdate()
 
 
 def getBuyBoxes():
-    infos = []
-    infos += tr_Product().cronBuyBox()
-    infos += hb_Product().cronBuyBox()
+    infos = ProductModule().cronBuyBox()
 
     if len(infos) > 0:
         loseBuyboxMail(infos)
