8e64d0f5b9c25a2c7db77a968bc2fd48dfb92b3b
iboliq <ibrahimdinc1999@gmail.com>
2021-06-05 22:19:03 +0300

    asd

--- wix_api/views.py
+++ wix_api/views.py
@@ -1,14 +1,16 @@
 from django.http.response import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 # Create your views here.
 
 
 def installWix(request):
-    installUrl = "https://www.wix.com/installer/install?appId=54954e33-345e-4eef-af36-1fe998a7a9e3&token={}".format(
+    installUrl = "https://www.wix.com/installer/install?appId=54954e33-345e-4eef-af36-1fe998a7a9e3&redirectUrl={}&token={}".format(
+        "https://dev.petifest.com/wix-auth",
         request.GET.get("token") if request.GET.get("token") else ""
     )
     return HttpResponseRedirect(installUrl)
+
 
 def authWix(request):
     content = """
