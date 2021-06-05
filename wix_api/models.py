c61130cdda2f720063c4d361061933979a7fc45a
iboliq <ibrahimdinc1999@gmail.com>
2021-06-06 00:30:55 +0300

    asd

--- wix_api/models.py
+++ wix_api/models.py
@@ -4,7 +4,7 @@ from marketplace.models import MarketProductModel
 
 
 class WixAuthTokensModel(models.Model):
-    refreshToken = models.CharField(verbose_name="Refresh Token", max_length=1000)
-    authToken = models.CharField(verbose_name="Auth Token", max_length=1000)
+    refreshToken = models.CharField(verbose_name="Refresh Token", max_length=499)
+    authToken = models.CharField(verbose_name="Auth Token", max_length=499)
     time = models.DateTimeField("Token Alma Tarihi", auto_now_add=True)
 
