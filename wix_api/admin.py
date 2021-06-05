df0f2f253b96fa0a8b419b6f11d648f763e7ca37
iboliq <ibrahimdinc1999@gmail.com>
2021-06-06 00:05:47 +0300

    wix auth completed

--- wix_api/admin.py
+++ wix_api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from wix_api.models import WixAuthTokensModel
 
 # Register your models here.
+
+
+@admin.register(WixAuthTokensModel)
+class WixAuthTokensModelAdmin(admin.ModelAdmin):
+    pass
--- wix_api/w_api.py
+++ wix_api/w_api.py
@@ -3,6 +3,8 @@ import requests, json
 client_id = "54954e33-345e-4eef-af36-1fe998a7a9e3"
 client_secret = "c650239b-38fc-4460-8bb7-ffa97562280f"
 
+
+
 def requestAccessToken(grantType, code):
     data = {
         "grant_type": grantType,
