1081dafc910689f10711dbdc0c69bfae425a839f
iboliq <ibrahimdinc1999@gmail.com>
2021-06-07 00:38:39 +0300

    asd

--- errorLogger/admin.py
+++ errorLogger/admin.py
@@ -1,3 +1,9 @@
+from errorLogger.models import ErrorLoggingModel
 from django.contrib import admin
 
 # Register your models here.
+
+
+@admin.register(ErrorLoggingModel)
+class ErrorLoggingModelAdmin(admin.ModelAdmin):
+    pass
